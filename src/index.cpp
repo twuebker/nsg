3871f7f7ebda4660f0c23a7dc5d4ce09dfd6c5e4
Theodor Wübker <theo@jmop.de>
2025-08-29 18:18:20 +0200

    Build IP into constructor

--- src/index.cpp
+++ src/index.cpp
@@ -10,6 +10,7 @@ Index::Index(const size_t dimension, const size_t n, Metric metric = L2)
     switch (metric) {
       case L2:distance_ = new DistanceL2();
         break;
+      case INNER_PRODUCT = new DistanceInnerProduct();
       default:distance_ = new DistanceL2();
         break;
     }
